292b42c8a49063f1aa5d79edaa647170995601b5
SvetaArtemyeva <sveta.artemeva.98@mail.ru>
2026-03-07 18:55:15 +0300

    hw05 - попытка №1

---
+++ hw-05/src/main/java/ru/otus/hw/repositories/JdbcAuthorRepository.java
@@ -0,0 +1,50 @@
+package ru.otus.hw.repositories;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
+import org.springframework.stereotype.Repository;
+import ru.otus.hw.models.Author;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcAuthorRepository implements AuthorRepository {
+
+    private final NamedParameterJdbcOperations jdbc;
+
+    @Override
+    public List<Author> findAll() {
+        return jdbc.query("select id, full_name from authors",
+            new AuthorRowMapper());
+    }
+
+    @Override
+    public Optional<Author> findById(long id) {
+        MapSqlParameterSource parameter = new MapSqlParameterSource()
+            .addValue("id", id);
+        try {
+            Author author = jdbc.queryForObject("select id, full_name from authors where id = :id",
+                parameter, new AuthorRowMapper());
+            return Optional.of(author);
+        } catch (EmptyResultDataAccessException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static class AuthorRowMapper implements RowMapper<Author> {
+
+        @Override
+        public Author mapRow(ResultSet rs, int i) throws SQLException {
+            long id = rs.getLong("id");
+            String fullName = rs.getString("full_name");
+            return new Author(id, fullName);
+        }
+    }
+}
