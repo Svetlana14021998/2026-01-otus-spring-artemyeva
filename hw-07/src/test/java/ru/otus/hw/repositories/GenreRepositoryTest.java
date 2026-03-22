d1cdb5ae385d37a726165c88f550bab5f11f6b4a
SvetaArtemyeva <sveta.artemeva.98@mail.ru>
2026-03-22 16:22:18 +0300

    hw07 - первая версия

---
+++ hw-07/src/test/java/ru/otus/hw/repositories/GenreRepositoryTest.java
@@ -0,0 +1,51 @@
+package ru.otus.hw.repositories;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.jdbc.Sql;
+import ru.otus.hw.models.Genre;
+
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Sql(scripts = "/db/genre-data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@DisplayName("Проверка работы GenreRepository")
+class GenreRepositoryTest extends AbstractRepositoryTest {
+
+    private static final int GENRES_COUNT = 4;
+    private static final long FIRST_GENRE_ID = 1;
+    private static final long THIRD_GENRE_ID = 3;
+
+    @Autowired
+    private GenreRepository repository;
+
+    @Test
+    @DisplayName("Поиск жанров по списку id-шников")
+    void findGenresByIdsTest() {
+        //given
+        //when
+        List<Genre> genres = repository.findAllByIdIn(Set.of(FIRST_GENRE_ID, THIRD_GENRE_ID));
+
+        //then
+        Genre genre1 = em.find(Genre.class, FIRST_GENRE_ID);
+        Genre genre3 = em.find(Genre.class, THIRD_GENRE_ID);
+        List<Genre> expectedGenres = List.of(genre1, genre3);
+
+        assertThat(genres).isNotEmpty();
+        assertThat(genres).usingRecursiveComparison().isEqualTo(expectedGenres);
+    }
+
+    @Test
+    @DisplayName("Поиск всех авторов")
+    void findAllGenresTest() {
+        //given
+        //when
+        List<Genre> genres = repository.findAll();
+
+        //then
+        assertThat(genres).hasSize(GENRES_COUNT);
+    }
+}
---
+++ hw-07/src/test/java/ru/otus/hw/services/BookServiceImplTest.java
@@ -0,0 +1,78 @@
+package ru.otus.hw.services;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.otus.hw.dto.BookDto;
+import ru.otus.hw.models.Book;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@DisplayName("Проверка работы BookServiceImpl")
+public class BookServiceImplTest extends AbstractServiceImplTest {
+
+    @Autowired
+    private BookServiceImpl bookService;
+
+    @Test
+    @DisplayName("Проверка загрузки всех связанных сущностей при поиске по id")
+    void doesNotThrowExceptionForFindByIdTest() {
+        // given
+        // when
+        Optional<BookDto> book = bookService.findById(1);
+
+        // then
+        assertThat(book).isPresent();
+
+        BookDto expectedBook = book.get();
+
+        assertAll(
+            () -> assertDoesNotThrow(() -> expectedBook.getAuthor().getFullName()),
+            () -> assertDoesNotThrow(() -> expectedBook.getGenres().size()));
+    }
+
+    @Test
+    @DisplayName("Проверка загрузки всех связанных сущностей при поиске всех книг")
+    void doesNotThrowExceptionForFindAllTest() {
+        // given
+        // when
+        List<BookDto> books = bookService.findAll();
+
+        // then
+        assertAll(
+            () -> assertDoesNotThrow(() -> books.get(0).getAuthor().getFullName()),
+            () -> assertDoesNotThrow(() -> books.get(0).getGenres().size()));
+    }
+
+    @Test
+    @DisplayName("Проверка загрузки всех связанных сущностей при сохранении книги")
+    void doesNotThrowExceptionForSaveTest() {
+        // given
+        // when
+        BookDto book = bookService.insert("new Book", 1, Set.of(1L, 2L));
+
+        // then
+        assertAll(
+            () -> assertDoesNotThrow(() -> book.getAuthor().getFullName()),
+            () -> assertDoesNotThrow(() -> book.getGenres().size()));
+    }
+
+    @Test
+    @DisplayName("Проверка загрузки всех связанных сущностей при изменении книги")
+    void doesNotThrowExceptionForUpdateTest() {
+        // given
+        // when
+        BookDto book = bookService.update(1, "update Book", 2, Set.of(3L, 4L));
+
+        // then
+        assertAll(
+            () -> assertDoesNotThrow(() -> book.getAuthor().getFullName()),
+            () -> assertDoesNotThrow(() -> book.getGenres().size()));
+    }
+}
